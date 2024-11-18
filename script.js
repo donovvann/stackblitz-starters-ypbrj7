80d4c9b7bc5b39cb339e5b4e4d561a6ff321d8c2
donovvann <dnvnlam@gmail.com>
2024-11-18 10:12:20 -0500

    background colors

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 console.log('Hello!');
 
 const myVar = "this is a variable"
+
+const todaysDate = () => {
+    let d = new Date();
+    return "Today's date is" + d
+}
+const dateBlock = document.getElementById("dateBlock")
+dateBlock.innerText = todaysDate();
+
+const body = document.body
+const d = new Date();
+
+if (d.getHours() > 20 || d.getHours() < 8) {
+    body.style["backgroundColor"] = "black";
+    body.style["color"] = "white";
+} else {
+    body.style["backgroundColor"] = "white";
+    body.style["color"] = "black";
+}
