5c4a11ba7f8e879cc21d912db89e6cfd3f28801c
donovvann <dnvnlam@gmail.com>
2024-11-21 12:38:38 -0500

    creeating objects

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('Hello!');
+
+const todaysDate = () => {
+    let d = new Date();
+    return "Today's date is" + d
+} 
+
+const dateBlock = document.getElementById("dateBlock")
+dateBlock.innText = todaysDate();
+const body = document.body;
+const d = newDate();
+if (d.getHours() > 20 || d.getHours() < 9 ){
+    body.style["backgroundColor"] = "black";
+    body.style["color"] = "white";
+} else {
+    body.style["backgroundColor"] = "white";
+    body.style["color"] = "black";
+}
+
